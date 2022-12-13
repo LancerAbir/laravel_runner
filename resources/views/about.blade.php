514235d6477cb528bef40a3e4590c5a7870928ba
LancerAbir <anjan.o24@gmail.com>
2022-12-13 10:51:24 +0600

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>This is About Page </h3>
+    <a href="/">Back Home Page</a><br />
+    Hello, {{$name}}.<br />
+    The current UNIX timestamp is {{ date('l jS \of F Y h:i:s A') }}.
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Learning</title>
+</head>
+<body>
+    <h2>Kaka laravel shikhi</h2>
+
+    <a href="/about"> About </a>
+</body>
+</html>
